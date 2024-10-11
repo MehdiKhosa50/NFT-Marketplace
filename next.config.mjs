90eec351c6c92986b386b5d3de8abc8e1cb6e7fc
Muhammad Muntazir Mehdi <mehdikhosa50@gmail.com>
2024-10-11 15:11:00 +0500

    Added Pinata Cloud Path

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: ['gateway.pinata.cloud'],
+    },
+  }
+  export default nextConfig;
