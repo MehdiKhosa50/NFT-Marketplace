0dfa96f9bf5b765b8006a5db32289292dc97b090
Muhammad Muntazir Mehdi <mehdikhosa50@gmail.com>
2024-10-14 18:52:48 +0500

    Script to mint NFT

--- contracts/PDT_NFT.sol
+++ contracts/PDT_NFT.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SimpleNFT is ERC721URIStorage, Ownable {
+contract PDT_NFT is ERC721URIStorage, Ownable {
     uint256 public nextTokenId = 1;
     
     constructor() ERC721("SimpleNFT", "SNFT") Ownable(msg.sender) {}
