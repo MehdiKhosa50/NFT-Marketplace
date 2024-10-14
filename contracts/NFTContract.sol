945881675294776696f79f6d56a45231c35edcf1
Muhammad Muntazir Mehdi <mehdikhosa50@gmail.com>
2024-10-14 18:52:37 +0500

    Script for deploying MarketPlace Contract

--- contracts/NFTContract.sol
+++ contracts/NFTContract.sol
@@ -9,13 +9,13 @@ contract NFTMarketplace {
     struct Counter {
         uint256 value;
     }
-    
+
     Counter private _listingIds;
-    
+
     function increment(Counter storage counter) internal {
         counter.value += 1;
     }
-    
+
     function current(Counter storage counter) internal view returns (uint256) {
         return counter.value;
     }
@@ -34,24 +34,42 @@ contract NFTMarketplace {
     mapping(uint256 => Listing) public listings;
     mapping(address => mapping(uint256 => uint256)) public tokenIdToListingId;
     mapping(address => uint256[]) public sellerListings;
-    
+
     uint256[] public allListingIds;
-    
+
     // ReentrancyGuard: To prevent attacks
     bool private _locked;
-    
+
     modifier nonReentrant() {
         require(!_locked, "ReentrancyGuard: reentrant call");
         _locked = true;
         _;
         _locked = false;
     }
-    
+
     // Events to display logs on Blockchain
-    event NFTListed(uint256 indexed listingId, address indexed nftContract, uint256 indexed tokenId, address seller, uint256 price);
-    event NFTSold(uint256 indexed listingId, address indexed nftContract, uint256 indexed tokenId, address seller, address buyer, uint256 price);
-    event ListingCanceled(uint256 indexed listingId, address indexed nftContract, uint256 indexed tokenId, address seller);
-    
+    event NFTListed(
+        uint256 indexed listingId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address seller,
+        uint256 price
+    );
+    event NFTSold(
+        uint256 indexed listingId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address seller,
+        address buyer,
+        uint256 price
+    );
+    event ListingCanceled(
+        uint256 indexed listingId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address seller
+    );
+
     // To get Onwership of an NFT
     function onERC721Received(
         address,
@@ -61,11 +79,15 @@ contract NFTMarketplace {
     ) public virtual returns (bytes4) {
         return this.onERC721Received.selector;
     }
-    
+
     // List NFT
-    function listNFT(address _nftContract, uint256 _tokenId, uint256 _price) external nonReentrant {
+    function listNFT(
+        address _nftContract,
+        uint256 _tokenId,
+        uint256 _price
+    ) external nonReentrant {
         require(_price > 0, "Price must be greater than 0");
-        
+
         IERC721 nftContract = IERC721(_nftContract);
         require(nftContract.ownerOf(_tokenId) == msg.sender, "Not the owner");
 
@@ -95,14 +117,28 @@ contract NFTMarketplace {
         Listing storage listing = listings[_listingId];
         require(listing.isActive, "Listing is not active");
         require(msg.value == listing.price, "Incorrect price");
-        require(msg.sender != listing.seller, "Seller cannot buy their own NFT");
+        require(
+            msg.sender != listing.seller,
+            "Seller cannot buy their own NFT"
+        );
 
         listing.isActive = false;
-        
-        IERC721(listing.nftContract).safeTransferFrom(address(this), msg.sender, listing.tokenId);
+
+        IERC721(listing.nftContract).safeTransferFrom(
+            address(this),
+            msg.sender,
+            listing.tokenId
+        );
         payable(listing.seller).transfer(msg.value);
 
-        emit NFTSold(_listingId, listing.nftContract, listing.tokenId, listing.seller, msg.sender, msg.value);
+        emit NFTSold(
+            _listingId,
+            listing.nftContract,
+            listing.tokenId,
+            listing.seller,
+            msg.sender,
+            msg.value
+        );
     }
 
     // Cancel Listing
@@ -112,19 +148,30 @@ contract NFTMarketplace {
         require(listing.seller == msg.sender, "Not the seller");
 
         listing.isActive = false;
-        
-        IERC721(listing.nftContract).safeTransferFrom(address(this), listing.seller, listing.tokenId);
 
-        emit ListingCanceled(_listingId, listing.nftContract, listing.tokenId, msg.sender);
+        IERC721(listing.nftContract).safeTransferFrom(
+            address(this),
+            listing.seller,
+            listing.tokenId
+        );
+
+        emit ListingCanceled(
+            _listingId,
+            listing.nftContract,
+            listing.tokenId,
+            msg.sender
+        );
     }
 
-    // To get all Listed Item 
+    // To get all Listed Item
     function getListingIds() external view returns (uint256[] memory) {
         return allListingIds;
     }
 
     // To get specific Seller Listed Items
-    function getSellerListingIds(address seller) external view returns (uint256[] memory) {
+    function getSellerListingIds(
+        address seller
+    ) external view returns (uint256[] memory) {
         return sellerListings[seller];
     }
 }
