1ccc75c4c899918301e76cb7f17e77f5847c68ca
Muhammad Muntazir Mehdi <mehdikhosa50@gmail.com>
2024-10-17 15:58:07 +0500

    Deleted Index for export

--- components/index.js
+++
@@ -1,6 +0,0 @@
-import Navbar from "./Navbar/NavBar";
-import Home from "./Home/Home";
-// import ListNFT from "./ListNFT/ListNFT";
-// import Profile from "./Profile/Profile";
-
-export{Navbar, Home};
